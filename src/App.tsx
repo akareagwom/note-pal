2086a1c09321c6ebfb27fc686d310b802d2eb201
akareagwom <akareazikang20@gmail.com>
2024-08-10 21:10:30 +0100

    homepage input

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,26 @@
 // import viteLogo from '/vite.svg'
 // import './App.css'
 // import NavBar from './components/NavBar';
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { Box, Heading, Text, Input, Stack, Button } from '@chakra-ui/react';
 
 function App() {
 
   return (
-    <Box w='100%' h={'100vh'} bg={'#272140'}>
-     <Box>
-      <Heading>Remind me</Heading>
-      <Text>Notes taking made easy</Text>
+    <Box w='100%' display={'flex'} flexDir={'column'} alignItems={'center'} color='white' h={'100vh'} bg={'#272140'}>
+     <Box textAlign={'start'} mt={4}>
+      <Heading color={'white'}>Remind me</Heading>
+      <Text color={'#544c83'}>Note taking made easy</Text>
      </Box>
+     <Stack mt={20} spacing={3}>
+      <Text>Email address</Text>
+      <Input bg={'#544c83'} border={'none'} borderRadius={'10px'} w={'400px'} placeholder='medium size' size='md' />
+      <Text>Password</Text>
+      <Input bg={'#544c83'} border={'none'} borderRadius={'10px'} w={'400px'} placeholder='medium size' size='md' />
+      <Text>Repeat password</Text>
+      <Input bg={'#544c83'} border={'none'} borderRadius={'10px'} w={'400px'} placeholder='medium size' size='md' />
+      <Button bg={''}>Get Started!</Button>
+    
+</Stack>
 
       
     </Box>
