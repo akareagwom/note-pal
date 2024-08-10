76243b426474245dcf95c05030a8f4306fdd35c9
akareagwom <akareazikang20@gmail.com>
2024-08-10 21:17:35 +0100

    homepage finish

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,18 @@ function App() {
 
   return (
     <Box w='100%' display={'flex'} flexDir={'column'} alignItems={'center'} color='white' h={'100vh'} bg={'#272140'}>
-     <Box textAlign={'start'} mt={4}>
-      <Heading color={'white'}>Remind me</Heading>
+     <Box textAlign={'left'} mr={'140px'} mt={6}>
+      <Heading color={'white'} fontSize={'3rem'}>Remind me</Heading>
       <Text color={'#544c83'}>Note taking made easy</Text>
      </Box>
-     <Stack mt={20} spacing={3}>
+     <Stack mt={16} spacing={3}>
       <Text>Email address</Text>
       <Input bg={'#544c83'} border={'none'} borderRadius={'10px'} w={'400px'} placeholder='medium size' size='md' />
       <Text>Password</Text>
       <Input bg={'#544c83'} border={'none'} borderRadius={'10px'} w={'400px'} placeholder='medium size' size='md' />
       <Text>Repeat password</Text>
       <Input bg={'#544c83'} border={'none'} borderRadius={'10px'} w={'400px'} placeholder='medium size' size='md' />
-      <Button bg={''}>Get Started!</Button>
+      <Button bg={'#e53756'} borderRadius={'10px'} color={'white'}>Get Started!</Button>
     
 </Stack>
 
