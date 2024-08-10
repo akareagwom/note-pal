48c32c46a69d591ac1b424e46285fd013f898445
akareagwom <akareazikang20@gmail.com>
2024-08-10 20:45:26 +0100

    homepage heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
-import { useState } from 'react';
-import { Link, Outlet } from 'react-router-dom';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react';
+// import { Link, Outlet } from 'react-router-dom';
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 // import './App.css'
-import NavBar from './components/NavBar';
-import { Box } from '@chakra-ui/react';
+// import NavBar from './components/NavBar';
+import { Box, Heading, Text } from '@chakra-ui/react';
 
 function App() {
 
   return (
-    <Box w='100%' h={''} bg={'#272140'}>
-      <div className="">
-        <NavBar/>
-      </div>
+    <Box w='100%' h={'100vh'} bg={'#272140'}>
+     <Box>
+      <Heading>Remind me</Heading>
+      <Text>Notes taking made easy</Text>
+     </Box>
 
-      <main>
-        <Outlet />
-      </main>
+      
     </Box>
   )
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,24 +1,13 @@
 import { Link, Outlet } from 'react-router-dom';
+import NavBar from './NavBar';
 
 
 const Layout: React.FC = () => {
   return (
     <div>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <div className="">
+        <NavBar/>
+      </div>
 
       <main>
         <Outlet />
