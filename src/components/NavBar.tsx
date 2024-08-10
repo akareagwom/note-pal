920b25aae545560449f1e8fed1964d13e144a72a
akareagwom <akareazikang20@gmail.com>
2024-08-10 18:22:06 +0100

    nav component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+const NavBar =()=>{
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">Navbar</a>
+        </nav>
+    )
+}
+
+export default NavBar;
