ad1d8d0032fb0bbbf07ab06744766905713db1e3
akareagwom <akareazikang20@gmail.com>
2024-08-11 19:02:33 +0100

    card addnote

---
+++ src/notes/components/AddNotes.tsx
@@ -0,0 +1,11 @@
+import { Box, Button } from "@chakra-ui/react"
+
+const AddNotes =()=>{
+    return(
+        <Box>
+            <Button>+</Button>
+        </Box>
+    )
+}
+
+export default AddNotes;
