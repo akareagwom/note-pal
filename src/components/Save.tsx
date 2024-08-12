3aec92de883ad8ac48de007da62144d28d8fa7fb
akareagwom <akareazikang20@gmail.com>
2024-08-12 23:37:26 +0100

    save buttoncl

--- src/components/Save.tsx
+++ src/components/Save.tsx
@@ -10,7 +10,7 @@ const Save: React.FC<Props> = ({  }) => {
   return (
     <Box>
         <Icon as={IoIosClose} />
-        <Button bg={'#e53756'}>Save</Button>
+        <Button bg={'#e53756'} color={'white'}>Save</Button>
     </Box>
   );
 };
