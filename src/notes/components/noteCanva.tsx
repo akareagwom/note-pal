3606c3a21cd8432c34e96b7cbb75eff7c4a3425f
akareagwom <akareazikang20@gmail.com>
2024-08-12 23:40:56 +0100

    save import

--- src/notes/components/noteCanva.tsx
+++ src/notes/components/noteCanva.tsx
@@ -1,5 +1,6 @@
 import { Box, Textarea } from '@chakra-ui/react';
 import React from 'react';
+import Save from '../../components/Save';
 
 interface Props {
   
@@ -8,6 +9,7 @@ interface Props {
 const NoteCanva: React.FC<Props> = ({  }) => {
   return (
     <Box>
+        <Save/>
       <Textarea variant={'none'} border={'none'} outline={'none'} fontSize={'1.5rem'} placeholder='New simple text' />
     </Box>
   );
