b7788156662d3d3d4ea4d8728caeb21f1af036a3
akareagwom <akareazikang20@gmail.com>
2024-08-12 23:58:32 +0100

    list radio

--- src/notes/components/ListCanva.tsx
+++ src/notes/components/ListCanva.tsx
@@ -12,6 +12,8 @@ const ListCanva: React.FC<Props> = ({  }) => {
        <Save/>
        <Textarea color={'#272140'} variant={'none'} border={'none'} outline={'none'} fontSize={'1.5rem'} placeholder='New simple text' />
        <Radio/>
+       <Radio/>
+       <Radio/>
     </Box>
   );
 };
