c388f9e1d245e1fe757629ae364ee802c196e501
akareagwom <akareazikang20@gmail.com>
2024-08-12 23:59:23 +0100

    list radiogroup

--- src/notes/components/ListCanva.tsx
+++ src/notes/components/ListCanva.tsx
@@ -1,4 +1,4 @@
-import { Box, Radio, Textarea} from '@chakra-ui/react';
+import { Box, Radio, RadioGroup, Textarea} from '@chakra-ui/react';
 import React from 'react';
 import Save from '../../components/Save';
 
@@ -11,9 +11,11 @@ const ListCanva: React.FC<Props> = ({  }) => {
     <Box>
        <Save/>
        <Textarea color={'#272140'} variant={'none'} border={'none'} outline={'none'} fontSize={'1.5rem'} placeholder='New simple text' />
-       <Radio/>
-       <Radio/>
-       <Radio/>
+       <RadioGroup>
+            <Radio/>
+            <Radio/>
+            <Radio/>
+       </RadioGroup>
     </Box>
   );
 };
