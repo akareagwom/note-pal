8ded8169e33c2fe97c6af3858871277957cea53e
akareagwom <akareazikang20@gmail.com>
2024-08-12 23:47:45 +0100

    close fs

--- src/components/Save.tsx
+++ src/components/Save.tsx
@@ -9,7 +9,7 @@ interface Props {
 const Save: React.FC<Props> = ({  }) => {
   return (
     <Flex pr={4} justifyContent={'space-between'}>
-        <Icon as={IoIosClose} />
+        <Icon fontSize={20}  as={IoIosClose} />
         <Button borderRadius={20} h={6} bg={'#e53756'} color={'white'}>Save</Button>
     </Flex>
   );
