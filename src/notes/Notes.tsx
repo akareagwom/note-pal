ba584b2c43aac12cfa0377834049eb4a05a015c2
akareagwom <akareazikang20@gmail.com>
2024-08-12 23:57:44 +0100

    list cana

--- src/notes/Notes.tsx
+++ src/notes/Notes.tsx
@@ -1,13 +1,15 @@
 import { Box } from "@chakra-ui/react";
 import CardComp from "./components/CardComp";
 import NoteCanva from "./components/noteCanva";
+import ListCanva from "./components/ListCanva";
 
 const   Notes =()=>{
     return (
         <Box  h={'100vh'}>
-            <NoteCanva/>
+            {/* <NoteCanva/> */}
+            <ListCanva/>
            <Box bg={'#272140'}>
-            
+
              {/* <CardComp/> */}
             </Box> 
      
