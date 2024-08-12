292f9e2e802b965671997448b570403cb850ef36
akareagwom <akareazikang20@gmail.com>
2024-08-12 12:31:35 +0100

    side bar search

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,8 @@ import { Box, Drawer, DrawerBody, List, ListItem ,DrawerFooter,
     DrawerCloseButton,
     Button,
     useDisclosure,
-    Input
+    Input,
+    Stack
   } from "@chakra-ui/react";
  
 
@@ -14,9 +15,13 @@ import { Box, Drawer, DrawerBody, List, ListItem ,DrawerFooter,
 
 const NavBar =()=>{
     return (
-        <Box h={'100vh'} bg={'#544c83'}>
-            <Input p={0} borderRadius={20} h={8} w={'44'} bg={'#272140'} />
-            <Button p={0}  borderRadius={20} h={8} w={'44'}>New Note</Button>
+        <Box pl={6} h={'100vh'} bg={'#544c83'}>
+            <Input placeholder="Search note" border={0} mt={4} color={'gray'} textAlign={'center'} fontSize={12} p={0} borderRadius={20} h={8} w={'44'} bg={'#272140'} />
+            <Button bg={'#e53756'} color={'white'} p={0} mt={4} borderRadius={20} fontSize={12} h={8} w={'44'}>New Note</Button>
+
+            <Stack>
+                
+            </Stack>
         </Box>
     )
 }
