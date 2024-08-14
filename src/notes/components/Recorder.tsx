e0162882ca99825494133a2ef50e502842847915
akareagwom <akareazikang20@gmail.com>
2024-08-14 22:25:33 +0100

    test record

---
+++ src/notes/components/Recorder.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+  
+}
+
+const Recorder: React.FC<Props> = ({  }) => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Recorder;
