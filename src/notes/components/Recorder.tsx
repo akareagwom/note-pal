f575ca927b43e3ecba82f285080d0ebcd975ba37
akareagwom <akareazikang20@gmail.com>
2024-08-15 13:28:21 +0100

    recording controls

--- src/notes/components/Recorder.tsx
+++ src/notes/components/Recorder.tsx
@@ -1,28 +1,30 @@
 import { Box, Button } from '@chakra-ui/react';
 import React from 'react';
 import { ReactMediaRecorder } from 'react-media-recorder';
-import WaveSurfer from 'wavesurfer.js';
+// import WaveSurfer from 'wavesurfer.js';
 
 interface Props {
   
 }
 
-const wavesurfer = WaveSurfer.create({
-    container: '#waveform',
-    waveColor: '#4F4A85',
-    progressColor: '#383351',
-    url: '/audio.mp3',
-  })
+// const wavesurfer = WaveSurfer.create({
+//     container: '#waveform',
+//     waveColor: '#4F4A85',
+//     progressColor: '#383351',
+//     url: '/audio.mp3',
+//   })
 
 const Recorder: React.FC<Props> = ({  }) => {
   return (
     <Box>
       <ReactMediaRecorder 
       audio
-       render={({startRecording,stopRecording  })=>(
+       render={({startRecording,stopRecording, mediaBlobUrl  })=>(
        <Box>
-        <Button onClick={startRecording}></Button>
-        <Button onClick={stopRecording}></Button>
+        <Button onClick={startRecording}>start</Button>
+        <Button onClick={stopRecording}>stop</Button>
+        {/* <Audio src={}/> */}
+        <audio src={mediaBlobUrl} controls></audio>
        </Box>
       )}/>
     </Box>
