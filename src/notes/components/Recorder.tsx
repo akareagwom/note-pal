fbcde45922a6912daad4a14cc376b2d92cfee428
akareagwom <akareazikang20@gmail.com>
2024-08-15 11:16:45 +0100

    start recording

--- src/notes/components/Recorder.tsx
+++ src/notes/components/Recorder.tsx
@@ -1,14 +1,25 @@
 import { Box } from '@chakra-ui/react';
 import React from 'react';
+import { ReactMediaRecorder } from 'react-media-recorder';
+import WaveSurfer from 'wavesurfer.js';
 
 interface Props {
   
 }
 
+const wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: '#4F4A85',
+    progressColor: '#383351',
+    url: '/audio.mp3',
+  })
+
 const Recorder: React.FC<Props> = ({  }) => {
   return (
     <Box>
-      
+      <ReactMediaRecorder render={({startRecording,  })=>(
+        <div id="waveform" />
+      )}/>
     </Box>
   );
 };
