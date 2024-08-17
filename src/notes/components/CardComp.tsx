f5da0db3c134ccc73542bb7db7c51eef38a5b4ce
akareagwom <akareazikang20@gmail.com>
2024-08-17 14:17:03 +0100

    cards component update

--- src/notes/components/CardComp.tsx
+++ src/notes/components/CardComp.tsx
@@ -19,37 +19,6 @@ const CardComp: React.FC<Props> =({ cards})=>{
                     </CardBody>
                 </Card>
             ))}
-            {/* <Card borderRadius={15} w={'45%'} p={4} bg={'rgba(215,238,248,255)'}>
-                <CardBody>
-                    <Text color={'#272140'}>hola</Text>
-                </CardBody>
-            </Card>
-            <Card borderRadius={15} w={'45%'} p={4} bg={'#f8d7d7'}>
-                <CardBody>
-                    <Text color={'#272140'}>hola</Text>
-                </CardBody>
-            </Card>
-            <Card borderRadius={15} w={'45%'} p={4} bg='rgba(218,187,250,255)'>
-                <CardBody >
-                    <Text color={'#272140'}>hola</Text>
-                </CardBody>
-            </Card>
-            <Card borderRadius={15} w={'45%'} p={4} bg={'rgba(215,238,248,255)'}>
-                <CardBody>
-                    <Text color={'#272140'}>hola</Text>
-                </CardBody>
-            </Card>
-            <Card borderRadius={15} w={'45%'} p={4} bg={'#f8d7d7'}>
-                <CardBody>
-                    <Text color={'#272140'}>hola</Text>
-                </CardBody>
-            </Card>
-            <Card borderRadius={15} w={'45%'} p={4} bg='rgba(218,187,250,255)'>
-                <CardBody >
-                    <Text color={'#272140'}>hola</Text>
-                </CardBody>
-            </Card>
-            */}
             <Box>
                 <AddNotes/>
             </Box>
