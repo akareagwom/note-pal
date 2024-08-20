bdc24d68fdd686d5b14fc7a472d2e66d85af9b04
akareagwom <akareazikang20@gmail.com>
2024-08-20 22:48:50 +0100

    addlist

---
+++ src/list/components/AddList.tsx
@@ -0,0 +1,38 @@
+import { Box, Button, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Icon } from "@chakra-ui/react";
+import { FaArrowRight } from "react-icons/fa";
+
+import React from 'react';
+
+
+
+const AddNotes: React.FC = ({  }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+
+  return (
+    <Box textAlign={'center'}>
+      <Button onClick={onOpen} mt={6} mx={40} bg={'#e53756'} color={'white'}>+</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add New</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody display={'inline-block'} justifyContent={'start'} textAlign={'left'} alignItems={''} lineHeight={2}>
+            <Button variant={'none'}>Simple note</Button>
+            <Button variant={'none'}>List</Button>
+            <Button variant={'none'}>Record note</Button>
+            <Button variant={'none'}>Sketch</Button>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button bg={'#e53756'} color={'white'} mr={3} onClick={onClose}>
+              <Icon as={FaArrowRight}/>
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Box>
+  );
+};
+
+export default AddNotes;
--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import Layout from './components/Layout.tsx'
 import Notes from './notes/Notes.tsx'
 import List from './list/List.tsx'
+import NoteCanva from './notes/components/noteCanva.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -17,6 +18,7 @@ createRoot(document.getElementById('root')!).render(
           <Route path='/Layout' element ={<Layout/>}>
             <Route index element ={<Notes/>}/>
             <Route path="list" element={<List />} />
+            {/* <Route path="noteCanva" element={<NoteCanva  cards={cards} setCards={setCards} note={note} setNote={setNote} />} /> */}
           </Route>
         </Routes>
       </BrowserRouter>
