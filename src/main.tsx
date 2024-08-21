0b5bb014f82a12732658f87f81c5d3a3d945a73c
akareagwom <akareazikang20@gmail.com>
2024-08-21 11:27:29 +0100

    refactored main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,27 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 // import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
-import Layout from './components/Layout.tsx'
-import Notes from './notes/Notes.tsx'
-import List from './list/List.tsx'
-import NoteCanva from './notes/components/noteCanva.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ChakraProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path='/Layout' element ={<Layout/>}>
-            <Route index element ={<Notes/>}/>
-            <Route path="list" element={<List />} />
-            {/* <Route path="noteCanva" element={<NoteCanva  cards={cards} setCards={setCards} note={note} setNote={setNote} />} /> */}
-          </Route>
-        </Routes>
-      </BrowserRouter>
-
+      <App/>
     </ChakraProvider>
   </StrictMode>,
 )
