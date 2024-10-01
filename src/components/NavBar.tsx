d4f092e14c2402bfb941001d4c9df5babbe5f2bf
akareagwom <akareazikang20@gmail.com>
2024-10-01 23:13:24 +0100

    mobile tab

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -22,9 +22,9 @@ import { Link } from "react-router-dom";
 
 const NavBar =()=>{
     return (
-        <Box pl={6}  h={'100vh'} bg={'#544c83'}>
+        <Box pl={6}  h={'100vh'} bg={{lg:'#544c83'}}>
             <Flex w={'200px'} justifyContent={'space-between'} pt={4} alignItems={'center'}>
-                <Flex  alignItems={'center'}>
+                <Flex display={{base:'none'}}  alignItems={'center'}>
                     <Avatar w={7} h={7} src='https://bit.ly/dan-abramov' />
                     <Text fontSize={14} ml={2}  color={'white'}>Akare</Text>
                 </Flex>
@@ -35,11 +35,11 @@ const NavBar =()=>{
             </Flex>
 
             <Box>
-                <Input placeholder="Search note" border={0} mt={4} color={'gray'} textAlign={'center'} fontSize={12} p={0} borderRadius={20} h={8} w={'52'} bg={'#272140'} />
+                <Input placeholder="Search note" border={0} mt={4} color={'gray'} textAlign={'center'} fontSize={12} p={5} borderRadius={{lg:20,base:10}} h={8} w={'52'} bg={{lg:'#272140',base:'#544c83'}} />
                 <Button bg={'#e53756'} color={'white'} p={0} mt={4} borderRadius={20} fontSize={12} h={8} w={'52'}>New Note</Button>
             </Box>
 
-            <Stack lineHeight={2} mt={6} color={'white'}>
+            <Stack display={{base:'none',md:'block'}} lineHeight={2} mt={6} color={'white'}>
                 <Link to='/Layout' >
                     <Flex alignItems={'center'}  >
                     <Icon color={'white'} as={FaStickyNote}/>
@@ -69,7 +69,7 @@ const NavBar =()=>{
                     <Text ml={4} fontSize={15}>Trash</Text>
                 </Flex>
             </Stack>
-            <Tabs>
+            <Tabs display={{base:'',md:'none'}}>
                 <TabList>
                     <Tab>Notes</Tab>
                     <Tab>List</Tab>
